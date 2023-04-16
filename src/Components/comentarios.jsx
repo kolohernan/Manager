721fd09545409f3cc2b5c483c5fb4986491ad9cb
kolohernan <kolohernan@gmail.com>
2023-04-16 19:59:59 -0300

    cambie algo en el h1

--- src/Components/comentarios.jsx
+++ src/Components/comentarios.jsx
@@ -4,7 +4,7 @@ import C_Avatar from './avatar';
 const C_comentario = ({p_sujeto}) => {
     return (
         <Fragment>
-            <h1>Comentarios</h1>
+            <h1>Caja de comentarios</h1>
             <div className="d-flex">
                 <div className="flex-shrink-0 ms-3">
                     <C_Avatar urlImagen={p_sujeto.urlImagen}/>
